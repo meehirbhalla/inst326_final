f7e72ff9779670e02be19ebf1fe778c8b985315a
Brice Tikum <bricetikum@MacBook-Pro-3.local>
2022-04-13 17:16:32 -0400

    Skeleton

--- final.py
+++ final.py
@@ -1,32 +1,63 @@
 import argparse
 
-<<<<<<< HEAD
 
 class HumanPlayer:
+    """Represents a Human player
     
+    Attributes:
+        name(str): Name of current player 
+    """
     def __init__(self):
+        """Function that will initialize the objects that were represented in
+           the attributes.
+           
+        Args:
+        
+        """
         pass
     
     def __add__(self):
+        """Used to add the scores at the end of each round
+        
+        Args:
+        
+        """
         pass
 
     def windStrength(self):
+        """Determines the strength and direction at which the wind is occuring
+        
+        Args:
+        
+        """
         pass
     
     def coordinates(self):
+        """The players chosen coordinates that the arrow is aimed and fired at
+        
+        Args:
+        
+        """
         pass
     
     def rounds(self):
-       pass 
+        """The rounds which are played in the game 
+        
+        Args:
+        
+        """
+        pass 
+   
+    
         
 class ComputerPlayer(HumanPlayer):
+    """Represents a computer player 
+
+    Args:
+        HumanPlayer (_type_): _description_
+    """
     
     def __init__(self):
         super().__init__()
-    
-=======
-# Meehir
-#Raeen
-#Brice
-#Khaliil
->>>>>>> b6a997e125fabf97c0bc7fdf1d49906470b08d8b
+
+
