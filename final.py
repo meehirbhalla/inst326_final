3091b3ec06bd570511fa40dfaf74e62848dde864
Brice Tikum <bricetikum@MacBook-Pro-3.local>
2022-04-13 16:58:33 -0400

    Skeleton

--- final.py
+++ final.py
@@ -1,5 +1,25 @@
-# INST326 final project
+import argparse
 
-# Meehir
-#Brice 
-#Brice Number 2 
+
+class HumanPlayer:
+    
+    def __init__(self):
+        pass
+    
+    def __add__(self):
+        pass
+
+    def windStrength(self):
+        pass
+    
+    def coordinates(self):
+        pass
+    
+    def rounds(self):
+       pass 
+        
+class ComputerPlayer(HumanPlayer):
+    
+    def __init__(self):
+        super().__init__()
+    
