f9204e72af99b6dbf681f860c0dbb4c9899320ee
Brice Tikum <bricetikum@MacBook-Pro-3.local>
2022-05-04 17:48:16 -0400

    Testing commit

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 import argparse
 import sys
-
+Rounds = 0
 class HumanPlayer():
     """Represents a Human player
     
@@ -9,7 +9,7 @@ class HumanPlayer():
         score (int): score of player
     """
     # utilizes optional parameters of name
-    def __init__(self, name = 'Player 1'):
+    def __init__(self, name = 'Player 1'): #KHALIIL
         """Function that will initialize the objects that were represented in
            the attributes.
            
@@ -24,7 +24,7 @@ class HumanPlayer():
         pass
 
     # __iadd__ magic method to add to dictionary of scores per round
-    def __iadd__(self, other):
+    def __iadd__(self, other):#BRICE
         """Used to add the scores at the end of each round. Total score per game
         will be stored in a dictionary with a key being a game # and value being
         the total score. 
@@ -35,11 +35,13 @@ class HumanPlayer():
         Return:
             dictionary of scores
         """
+        score_per_game = dict()
+        
          # uses the __iadd__ magic method to calculate the score and add to
          # dictionary of scores each round
         pass
     
-    def coordinates(self):
+    def coordinates(self): #MEHIR
         """The players inputted coordinates which the arrow is aimed and fired 
         at. Coordinate is determined based on relative position of shot to 
         center and accounts wind interference.
@@ -87,9 +89,18 @@ class ComputerPlayer(HumanPlayer):
         to shoot.
         """         
         # overrides the turn method in the human class since computer turn randomly generates a coordinate to shoot
+<<<<<<< HEAD
 
+=======
+        
+def round():#KHALIIL
+    """Initiates one round of the game.
+    """
+    Rounds = Rounds + 1
+    pass
+>>>>>>> bcf7223 (Testing commit)
 
-def wind_strength():
+def wind_strength():#MEHIR
     """Determines the strength and direction at which the wind is occuring.
     
     Return:
@@ -97,7 +108,7 @@ def wind_strength():
     """
     # randomly generate wind direction of 1-2 units in NESW direction
 
-def validate_shot():
+def validate_shot(): #MEHIR
     """Determines distance from bullseye.
     
     Return:
@@ -110,26 +121,44 @@ def validate_shot():
     #center
     pass
 
-def game_over():
+def game_over():#BRICE
     """Game is over and determines the winner. 
     
     Return:
         boolean: false if game is not over true if game is over
     """
+    player = HumanPlayer()
+    #May need a function to determine winner 
+    #How do I call score_per_game dict from here
+    #best_score = max(player.score_per_game, key=player.score_per_game.get)
+    #best_score = player.score_per_game.sort(key=lambda x: )
+    if Rounds == 3:
+        
+        print(f"The winner is: {player.name} with a total score of: {player.__iadd__()}")
+        print()
+        return True
+    else:
+        return False
     # use f-strings to display the name and total score over all 3 rounds
     # use custom list sorting to list best performing rounds by score
+
+def winner(player):
     
-def main():
+    pass
+
+    
+def main():#BRICE
     """Plays one round of the archery game and calls necessary 
     methods/functions.
     """
+    #Isnt this doing the same thing as round()?
     # Will make a call to coordinates(), turn(), validate_shot(), score(), and 
     # game_over()
     pass 
 
 # argument parser in order to use command line arguments 
 # (player name and desired target) 
-def parse_args(arglist):
+def parse_args(arglist):#Brice
     """Parse command line arguments.
     
     Expect two mandatory arguments:
@@ -142,7 +171,10 @@ def parse_args(arglist):
     Returns:
         namespace: parsed arguments
     """ 
-    pass
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Player1", help= "Name ofthe first player")
+    parser.add_argument("Computer", help= "Name ofthe computer player")
+    return parser.parse_args(arglist)
+ 
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
