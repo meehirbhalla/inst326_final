23a56414bb517f76c05c112eaf8696a03eb4c12b
Meehir Bhalla <meehirjbhalla@gmail.com>
2022-05-05 18:33:20 -0400

    edit - meehir

--- final.py
+++ final.py
@@ -2,6 +2,35 @@ import argparse
 import sys
 import random
 
+# valid positions on the target
+POTENTIAL_SPOTS = {
+    'A1': 1, 
+    'A2': 2,
+    'A3': 3,
+    'A4': 4,
+    'A5': 5,
+    'B1': 6,
+    'B2': 7,
+    'B3': 8,
+    'B4': 9,
+    'B5': 10,
+    'C1': 11,
+    'C2': 12,
+    'C3': 13,
+    'C4': 14,
+    'C5': 15,
+    'D1': 16,
+    'D2': 17,
+    'D3': 18,
+    'D4': 19,
+    'D5': 20,
+    'E1': 21,
+    'E2': 22,
+    'E3': 23,
+    'E4': 24,
+    'E5': 25
+}
+
 class HumanPlayer():
     """Represents a Human player
     
