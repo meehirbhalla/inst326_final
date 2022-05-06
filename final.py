4e62bab4fa104891bc9ce7c5577f71cb4d89b393
Meehir Bhalla <meehirjbhalla@gmail.com>
2022-05-05 23:51:59 -0400

    meehir

--- final.py
+++ final.py
@@ -148,6 +148,61 @@ class HumanPlayer():
             self.selected_coordinate = int(str('5') + str(y))
         
         print (f'Coordinate selected: ,{player_input}')
+    
+    def wind_strength():#MEHIR
+    """Determines the direction at which the wind is occuring.
+        
+        Return:
+            wind strength which consists of direction
+        """
+        # list of potential directions
+        direction = ['N', 'S', 'E', 'W']
+    
+        # if A is the X coordinate the player selected
+        if (self.selected_coordinate == 11):
+            self.random_direction = (random.choice(direction[0::2]))
+        elif (self.selected_coordinate == 12) or (self.selected_coordinate == 13) or (self.selected_coordinate == 14):
+            self.random_direction = (random.choice(direction[0::2]))
+        else:
+            self.random_direction = (random.choice(direction[1::2]))
+            
+        # if B, C, or D is the X coordinate the player selected
+        if (self.selected_coordinate == 21) or (self.selected_coordinate == 31) or (self.selected_coordinate == 41):
+            dont_include = 1
+            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
+        elif (self.selected_coordinate == 25) or (self.selected_coordinate == 35) or (self.selected_coordinate == 45):
+            dont_include = 0
+            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
+        else:
+            self.random_direction = (random.choice(direction))
+            
+        # if E is the X coordinate the player selected
+        if (self.selected_coordinate == 51):
+            self.random_direction = (random.choice(direction[0::3]))
+        elif (self.selected_coordinate == 52) or (self.selected_coordinate == 53) or (self.selected_coordinate == 54):
+            dont_include = 2
+            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
+        else:
+            self.random_direction = (random.choice(direction[1::3]))
+
+def validate_shot():
+    """Determines distance from bullseye.
+
+        Return:
+            affected coordinates distance from bullseye
+        """
+        bullseye = 33
+        
+        # unpack final coordinate
+        x,y = self.final_coordinate
+        
+        # determine distance using the x and y coordinate values of the final affected coordinate
+        if (x == 2) or (x == 3) or (x == 4) and (y == 2) or (y == 3) or (y == 4):
+            self.distance_to_bullseye = 1
+        elif (x == 3) and (y == 3):
+            self.distance_to_bullseye = 0
+        else:
+            self.distance_to_bullseye = 2
   
         
 class ComputerPlayer(HumanPlayer):
@@ -193,64 +248,7 @@ class ComputerPlayer(HumanPlayer):
             self.selected_coordinate = int(str('5') + str(y))
         
         print (f'Coordinate selected: ,{computer_selected}')
-        
-def wind_strength():#MEHIR
-    """Determines the direction at which the wind is occuring.
-        
-        Return:
-            wind strength which consists of direction
-        """
-        # list of potential directions
-        direction = ['N', 'S', 'E', 'W']
     
-        # if A is the X coordinate the player selected
-        if (self.selected_coordinate == 11):
-            self.random_direction = (random.choice(direction[0::2]))
-        elif (self.selected_coordinate == 12) or (self.selected_coordinate == 13) or (self.selected_coordinate == 14):
-            self.random_direction = (random.choice(direction[0::2]))
-        else:
-            self.random_direction = (random.choice(direction[1::2]))
-            
-        # if B, C, or D is the X coordinate the player selected
-        if (self.selected_coordinate == 21) or (self.selected_coordinate == 31) or (self.selected_coordinate == 41):
-            dont_include = 1
-            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
-        elif (self.selected_coordinate == 25) or (self.selected_coordinate == 35) or (self.selected_coordinate == 45):
-            dont_include = 0
-            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
-        else:
-            self.random_direction = (random.choice(direction))
-            
-        # if E is the X coordinate the player selected
-        if (self.selected_coordinate == 51):
-            self.random_direction = (random.choice(direction[0::3]))
-        elif (self.selected_coordinate == 52) or (self.selected_coordinate == 53) or (self.selected_coordinate == 54):
-            dont_include = 2
-            self.random_direction = (random.choice(direction[:dont_include] + direction[dont_include+1:]))
-        else:
-            self.random_direction = (random.choice(direction[1::3]))
-
-def validate_shot(): #MEHIR
-    """Determines distance from bullseye.
-
-        Return:
-            affected coordinates distance from bullseye
-        """
-        bullseye = 33
-        
-        # unpack final coordinate
-        x,y = self.final_coordinate
-        
-        # determine distance using the x and y coordinate values of the final affected coordinate
-        if (x == 2) or (x == 3) or (x == 4) and (y == 2) or (y == 3) or (y == 4):
-            self.distance_to_bullseye = 1
-        elif (x == 3) and (y == 3):
-            self.distance_to_bullseye = 0
-        else:
-            self.distance_to_bullseye = 2
-    
-    
-
 
 def game_over():
     """Game is over and determines the winner. 
