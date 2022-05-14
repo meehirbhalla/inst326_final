7669910e6dad305e28b6f5140f91cbaa0caedeba
Meehir Bhalla <meehirjbhalla@gmail.com>
2022-05-13 21:39:00 -0400

    comments

--- final.py
+++ final.py
@@ -147,7 +147,7 @@ class HumanPlayer():
         E = ['a1', 'a2', 'a3', 'a4', 'a5' , 'b1', 'b2', 'b3', 'b4', 'b5', 'c1', 'c2', 'c3', 'c4', 'c5', 'd1', 'd2', 'd3', 'd4', 'd5']
         W = ['b1', 'b2', 'b3', 'b4', 'b5', 'c1', 'c2', 'c3', 'c4', 'c5', 'd1', 'd2', 'd3', 'd4', 'd5', 'e1', 'e2', 'e3', 'e4', 'e5']
         
-        # prompts users for coordinate with restrictions applied
+        # prompts users for coordinate with restrictions applied and makes sure user input is valid
         if self.wind == 'North':
             self.player_input = input(f'{self.name}, please enter a coordinate in the format (xy), where x is a letter from A-E and y is a number from 1-4: ')
             while self.player_input.lower() not in N:
