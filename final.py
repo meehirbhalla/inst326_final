5cf7774531547f8f16d83989dd3cbbe59e46b1bf
raeenf <43077415+raeenf@users.noreply.github.com>
2022-05-16 20:26:04 -0400

    changes to score()

--- final.py
+++ final.py
@@ -89,6 +89,20 @@ class HumanPlayer():
         # 41 42 43 44 45
         # 51 52 53 54 55
         
+        x,y = self.final_coordinate
+        
+        if x == 'a':
+            self.final_coordinate = int(str('1') + str(y))
+        elif x  == 'b':
+            self.final_coordinate = int(str('2') + str(y))
+        elif x  == 'c':
+            self.final_coordinate = int(str('3') + str(y))
+        elif x  == 'd':
+            self.final_coordinate = int(str('4') + str(y))
+        elif x  == 'e':
+            self.final_coordinate = int(str('5') + str(y))
+        
+        
         if self.final_coordinate == 33:
             points = 10
         elif self.final_coordinate == 22 or self.final_coordinate == 23 or self.final_coordinate == 24 or self.final_coordinate == 32 or self.final_coordinate == 34 or self.final_coordinate == 42 or self.final_coordinate == 43 or self.final_coordinate == 44:
@@ -273,7 +287,7 @@ class ComputerPlayer(HumanPlayer):
         elif x == 'e':
             self.selected_computer_coordinate = int(str('5') + str(y))
         
-        
+        self.final_coordinate = computer_selected
         print (f'Coordinate selected: ,{computer_selected}')
     
 
